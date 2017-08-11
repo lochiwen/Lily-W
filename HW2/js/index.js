94b3c27d6e0945395831b4170281cfad18c4fa68
lochiwen <name.1911434@gmail.com>
2017-08-11 17:55:57 +0800

    4

--- HW2/js/index.js
+++ HW2/js/index.js
@@ -27,15 +27,18 @@ $('#query').on('click', function() {
 
                 // TODO HW2 作業寫在這裡
 
-                // 一開始先把商品的子元素清空
-                $('product-list').empty()
+                // 一開始先把放資料的位子清空
+                $('#product-list').empty()
+                $('img').empty()
+                $('h3').empty()
+                $('p').empty()
                     // 令 quantity 指商品資料數量
                 var quantity = response.data.length
                 for (var i = 0; i < quantity; i++) {
                     // 令商品資料庫內各項商品的圖片、商品名、價格資料為變數
-                    var image = $('<img>').attr({ class: thumb, src: response.data[i].image });
-                    var name = $('<h3>').attr({ class: title, src: response.data[i].name });
-                    var price = $('<p>').attr({ class: price, src: response.data[i].price });
+                    var image = $('img').attr({ class: thumb, src: response.data[i].image });
+                    var name = $('h3').attr({ class: title }).addClass("response.data[i].name");
+                    var price = $('p').attr({ class: price }).addClass("response.data[i].price");
                     // 把分別抓的變數集結放到同一資料來源的商品下
                     var eachproduct = $('<div>').attr('class', 'item').append(image).append(name).append(price);
                     // 商品列顯示資料齊全的商品
