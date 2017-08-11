9861aff59049aea8c3e4a082eb758be61abeee99
lochiwen <name.1911434@gmail.com>
2017-08-11 20:45:36 +0800

    2

--- HW2/js/index.js
+++ HW2/js/index.js
@@ -29,20 +29,21 @@ $('#query').on('click', function() {
 
                 // 一開始先把放資料的位子清空
                 $('#product-list').empty()
-                $('img').empty()
-                $('h3').empty()
-                $('p').empty()
-                    // 令 quantity 指商品資料數量
+                $('<div>').addClass("item").empty()
+                $('<div>').addClass("col-*").empty()
+
+                // 令 quantity 指商品資料數量
                 var quantity = response.data.length
                 for (var i = 0; i < quantity; i++) {
                     // 令商品資料庫內各項商品的圖片、商品名、價格資料為變數
-                    var image = $('img').attr({ class: thumb, src: response.data[i].image });
-                    var name = $('h3').attr({ class: title }).addClass("response.data[i].name");
-                    var price = $('p').attr({ class: price }).addClass("response.data[i].price");
+                    src = "http://fakeimg.pl/160/?text=images"
+                    var image = $('<img>').addClass("thumb").attr("src", response.data[i].image);
+                    var name = $('<h3>').addClass("title").text(response.data[i].name);
+                    var price = $('<p>').addClass("price").text("$NT " + response.data[i].price);
                     // 把分別抓的變數集結放到同一資料來源的商品下
-                    var eachproduct = $('<div>').attr('class', 'item').append(image).append(name).append(price);
+                    var eachproduct = $('<div>').addClass("item").append(image).append(name).append(price);
                     // 商品列顯示資料齊全的商品
-                    var showlist = $('<div>').attr('class', 'col-*').append(eachproduct);
+                    var showlist = $('<div>').addClass("col-*").append(eachproduct);
                     // 放到顯示商品的元件裡
                     $('#product-list').append(showlist);
 
